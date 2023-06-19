c845b32e16caf6b4591157eb97ce19385cbd629e
srezasm <srezas.2017@gmail.com>
2023-06-19 20:09:55 +0330

    daily problem

---
+++ 2023-06/1732.find-the-highest-altitude.cpp
@@ -0,0 +1,70 @@
+// 2023-06-19
+// such a nice problem to keep the streak on my exam night :)
+
+/*
+ * @lc app=leetcode id=1732 lang=cpp
+ *
+ * [1732] Find the Highest Altitude
+ *
+ * https://leetcode.com/problems/find-the-highest-altitude/description/
+ *
+ * algorithms
+ * Easy (79.33%)
+ * Likes:    2073
+ * Dislikes: 176
+ * Total Accepted:    180.8K
+ * Total Submissions: 221.1K
+ * Testcase Example:  '[-5,1,5,0,-7]'
+ *
+ * There is a biker going on a road trip. The road trip consists of n + 1
+ * points at different altitudes. The biker starts his trip on point 0 with
+ * altitude equal 0.
+ * 
+ * You are given an integer array gain of length n where gain[i] is the net
+ * gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n).
+ * Return the highest altitude of a point.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: gain = [-5,1,5,0,-7]
+ * Output: 1
+ * Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: gain = [-4,-3,-2,-1,4,3,2]
+ * Output: 0
+ * Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is
+ * 0.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * n == gain.length
+ * 1 <= n <= 100
+ * -100 <= gain[i] <= 100
+ * 
+ * 
+ */
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int ans = 0;
+        int cur = 0;
+        for(const auto& i: gain) {
+            cur += i;
+            ans = max(ans, cur);
+        }
+        return ans;
+    }
+};
