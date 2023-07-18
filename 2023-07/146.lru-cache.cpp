ff5e56ad6046362c60325ae2f678fd87bae82e1b
srezasm <srezas.2017@gmail.com>
2023-07-18 21:43:13 +0330

    problem 146

---
+++ 2023-07/146.lru-cache.cpp
@@ -0,0 +1,143 @@
+// 2023-07-18
+
+/*
+ * @lc app=leetcode id=146 lang=cpp
+ *
+ * [146] LRU Cache
+ *
+ * https://leetcode.com/problems/lru-cache/description/
+ *
+ * algorithms
+ * Medium (40.80%)
+ * Likes:    18509
+ * Dislikes: 829
+ * Total Accepted:    1.4M
+ * Total Submissions: 3.3M
+ * Testcase Example:  '["LRUCache","put","put","get","put","get","put","get","get","get"]\n' +
+  '[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]'
+ *
+ * Design a data structure that follows the constraints of a Least Recently
+ * Used (LRU) cache.
+ *
+ * Implement the LRUCache class:
+ *
+ *
+ * LRUCache(int capacity) Initialize the LRU cache with positive size
+ * capacity.
+ * int get(int key) Return the value of the key if the key exists, otherwise
+ * return -1.
+ * void put(int key, int value) Update the value of the key if the key exists.
+ * Otherwise, add the key-value pair to the cache. If the number of keys
+ * exceeds the capacity from this operation, evict the least recently used
+ * key.
+ *
+ *
+ * The functions get and put must each run in O(1) average time complexity.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input
+ * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * Output
+ * [null, null, null, 1, null, -1, null, -1, 3, 4]
+ *
+ * Explanation
+ * LRUCache lRUCache = new LRUCache(2);
+ * lRUCache.put(1, 1); // cache is {1=1}
+ * lRUCache.put(2, 2); // cache is {1=1, 2=2}
+ * lRUCache.get(1);    // return 1
+ * lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+ * lRUCache.get(2);    // returns -1 (not found)
+ * lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+ * lRUCache.get(1);    // return -1 (not found)
+ * lRUCache.get(3);    // return 3
+ * lRUCache.get(4);    // return 4
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= capacity <= 3000
+ * 0 <= key <= 10^4
+ * 0 <= value <= 10^5
+ * At most 2 * 10^5 calls will be made to get and put.
+ *
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class LRUCache {
+   public:
+    inline static int M[10001];
+    inline static int16_t L[10002][2];
+    int cap, size = 0;
+    const int NONE = 10001;
+    int head = NONE, tail = NONE;
+
+    LRUCache(int capacity) : cap(capacity) { memset(M, 0xff, sizeof M); }
+
+    inline void erase(int key) {
+        const int pre = L[key][0];
+        const int nxt = L[key][1];
+        L[pre][1] = nxt;
+        L[nxt][0] = pre;
+        if (head == key)
+            head = nxt;
+        if (tail == key)
+            tail = pre;
+    }
+
+    inline void push_front(int key) {
+        L[key][0] = NONE;
+        L[key][1] = head;
+        L[head][0] = key;
+        head = key;
+        if (tail == NONE)
+            tail = key;
+    }
+
+    inline int pop_back() {
+        int ret = tail;
+        tail = L[tail][0];
+        L[tail][1] = NONE;
+        if (tail == NONE)
+            head = NONE;
+        return ret;
+    }
+
+    int get(int key) {
+        if (M[key] == -1)
+            return -1;
+        erase(key);
+        push_front(key);
+        return M[key];
+    }
+
+    void put(int key, int value) {
+        if (M[key] != -1) {
+            erase(key);
+        } else {
+            if (size == cap) {
+                int poped = pop_back();
+                M[poped] = -1;
+                size -= 1;
+            }
+            size += 1;
+        }
+        push_front(key);
+        M[key] = value;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
