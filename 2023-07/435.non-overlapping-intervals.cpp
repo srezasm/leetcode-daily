772cb65836f4246bd6b1e7e79a5de931dd57ebda
srezasm <srezas.2017@gmail.com>
2023-07-19 20:12:23 +0330

    problem 435

---
+++ 2023-07/435.non-overlapping-intervals.cpp
@@ -0,0 +1,84 @@
+// 2023-07-19
+
+/*
+ * @lc app=leetcode id=435 lang=cpp
+ *
+ * [435] Non-overlapping Intervals
+ *
+ * https://leetcode.com/problems/non-overlapping-intervals/description/
+ *
+ * algorithms
+ * Medium (50.43%)
+ * Likes:    6894
+ * Dislikes: 181
+ * Total Accepted:    420.4K
+ * Total Submissions: 816.3K
+ * Testcase Example:  '[[1,2],[2,3],[3,4],[1,3]]'
+ *
+ * Given an array of intervals intervals where intervals[i] = [starti, endi],
+ * return the minimum number of intervals you need to remove to make the rest
+ * of the intervals non-overlapping.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+ * Output: 1
+ * Explanation: [1,3] can be removed and the rest of the intervals are
+ * non-overlapping.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: intervals = [[1,2],[1,2],[1,2]]
+ * Output: 2
+ * Explanation: You need to remove two [1,2] to make the rest of the intervals
+ * non-overlapping.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: intervals = [[1,2],[2,3]]
+ * Output: 0
+ * Explanation: You don't need to remove any of the intervals since they're
+ * already non-overlapping.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= intervals.length <= 10^5
+ * intervals[i].length == 2
+ * -5 * 10^4 <= starti < endi <= 5 * 10^4
+ *
+ *
+ */
+
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+   public:
+    static bool cmp(vector<int>& a, vector<int>& b) { return a[1] < b[1]; }
+
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), cmp);
+
+        int prev = 0;
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] >= intervals[prev][1]) {
+                prev = i;
+                count++;
+            }
+        }
+        return n - count;
+    }
+};
